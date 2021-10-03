d4bc5b590959e16e5322e5a71809aa05c036c82f
[folivora] <[info@folivora.design]>
2021-10-03 15:41:49 +0900

    simple crud

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'Frshmn3461#$',
+  database: 'employeesystem',
+});
+
+app.post('/create', (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    'INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)',
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("valuse inserted");
+      }
+    }
+  );
+});
+
+app.listen(3001, ()=> {
+  console.log("yes, your server is running on port 3001");
+});
